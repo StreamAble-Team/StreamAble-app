8d30e012d02cc2c77686a167bad2513fbbd989e4
Tdanks2000 <tommydanks2000@hotmail.com>
2022-11-19 18:31:03 +0000

    banner styles

--- src/components/Banner/Banner.styles.js
+++ src/components/Banner/Banner.styles.js
@@ -8,6 +8,7 @@ export const BannerContainer = styled.View`
   marginBottom: 20px;
   color: #fff;
   overflow: hidden;
+  position: relative;
 `;
 
 /* prettier-ignore */
@@ -15,6 +16,7 @@ export const BannerImage = styled.ImageBackground`
   width: 100%;
   height: 100%;
   resizeMode: cover;
+  
 `;
 
 /* prettier-ignore */
@@ -22,15 +24,16 @@ export const BannerContent = styled.View`
   backgroundColor: rgba(0, 0, 0, 0.6);
   height: 100%;
   width: 100%;
-  padding: 5px 10px;
+  padding: 15px 20px;
+  display: flex;
+  justifyContent: flex-end;
 `;
 
 /* prettier-ignore */
 export const BannerTitle = styled.Text`
   color: #fff;    
-  fontSize: 20px;
+  fontSize: 25px;
   fontFamily: OpenSans_800ExtraBold;
-  marginBottom: 8px;
 `;
 
 /* prettier-ignore */
@@ -38,4 +41,31 @@ export const BannerDescription = styled.Text`
   color: ${({ theme }) => theme.text.offWhite};
   fontsize: 15px;
   fontFamily: OpenSans_400Regular;
+`;
+
+/* prettier-ignore */
+export const BannerMeta = styled.View`
+  display: flex;
+  justifyContent: flex-start;
+  flexDirection: row;
+  alignItems: center;
+  marginTop: 17px;
+`;
+
+/* prettier-ignore */
+export const BannerMetaItem = styled.Text`
+  color: ${({ theme }) => theme.text.offWhite};
+  fontSize: 10px;
+  fontFamily: OpenSans_400Regular;
+  marginRight: 10px;
+`;
+
+/* prettier-ignore */
+export const BannerMetaDot = styled.View`
+  height: 3px;
+  width: 3px;
+  borderRadius: 50%;
+  backgroundColor: ${({ theme }) => theme.text.offWhite};
+  marginRight: 10px;
+  
 `;
