1c70a546f94947ac451c14075fbb08bf6911473f
Tdanks2000 <tommydanks2000@hotmail.com>
2022-12-11 07:50:14 +0000

    ✖️ - can now remove from anilist list

--- src/components/Modals/Info/RemoveFromList/index.js
+++ src/components/Modals/Info/RemoveFromList/index.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import React from "react";
 import { Container, RemoveIcon, RemoveText } from "./RemoveFromList.styles";
 import {
@@ -8,9 +8,29 @@ import {
   selectCollection,
   updateCollection,
 } from "../../../../database";
+import { useDebouncedMutation } from "../../../../hooks";
+import {
+  GetAnimeDocument,
+  refetchGetAnimeQuery,
+  RemoveFromListDocument,
+  useGetAnimeQuery,
+} from "../../../../utils/graphql/generated";
 
 const RemoveFromList = ({ data, setActiveIndex }) => {
   const subOrDub = data.subOrDub;
+
+  const {
+    loading,
+    data: anilistData,
+    refetch,
+    error,
+  } = useGetAnimeQuery({
+    variables: { id: data.id },
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const mediaListEntry = anilistData?.Media?.mediaListEntry || undefined;
+
   const removeFromCollection = async () => {
     const db = await getDBConnection();
     await createCollectionTable(db);
@@ -31,8 +51,61 @@ const RemoveFromList = ({ data, setActiveIndex }) => {
     }
   };
 
+  const removeFromAnilist = useDebouncedMutation({
+    mutationDocument: RemoveFromListDocument,
+    makeUpdateFunction: (_variables) => (proxy) => {
+      const proxyData = proxy.readQuery({
+        query: GetAnimeDocument,
+        variables: { id: data.id },
+      });
+
+      if (proxyData?.Media?.mediaListEntry) {
+        proxy.writeQuery({
+          query: GetAnimeDocument,
+          variables: { id: data.id },
+          data: {
+            ...proxyData,
+            Media: {
+              ...proxyData?.Media,
+              id: proxyData?.Media?.id,
+              mediaListEntry: mediaListEntry,
+            },
+          },
+        });
+      }
+    },
+    wait: 0,
+    refetchQueries: [refetchGetAnimeQuery({ id: data.id })],
+  });
+
+  const removeFromList = async () => {
+    await removeFromCollection();
+    if (mediaListEntry)
+      await removeFromAnilist({
+        id: mediaListEntry?.id,
+      });
+  };
+
+  const Alerted = async () => {
+    Alert.alert(
+      "Remove from list",
+      "Are you sure you want to remove this anime from your list?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Remove",
+          onPress: () => removeFromList(),
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
-    <Container onPress={removeFromCollection}>
+    <Container onPress={Alerted}>
       <RemoveIcon name="trash" />
       <RemoveText>Remove from list</RemoveText>
     </Container>
